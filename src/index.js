76458161de9268c79b03de58b64b3332207e201e
Daniel Pease <daniel.j.pease@gmail.com>
2016-11-30 10:21:16 -0500

    add gitignore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 
 import React    from 'react'
 import ReactDOM from 'react-dom'
-import App      from './Components/00App/App.jsx'
+import App      from './Components/App/App.jsx'
 
-// mount our App at #container
 ReactDOM.render(<App />, document.querySelector('#root-container'),
 
 );

925e8f9fe4fb411d3f2b7d30d1acfc4a0cf0b51e
Daniel Pease <daniel.j.pease@gmail.com>
2016-11-30 11:50:13 -0500

    fleshed out stuff

--- server.js
+++ server.js
@@ -14,11 +14,4 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// app.use(expressJWT({secret: process.env.secret}).unless(
-//   {path: ['/favicon.ico', '/user/signup', '/user/login', '/api/storefronts', '/api/myStorefront', '/api/storefront', '/api/items', '/search/zip']}
-// ));
-
-// const userRouter = require('./routes/user.js');
-// app.use('/user', userRouter);
-
 app.listen(PORT, () => {console.log('Overpriced organic artisanal tacos on port', PORT)});

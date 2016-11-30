d0bc73c56d3cd5d1b3a4af338847f4af417900f8
Daniel Pease <daniel.j.pease@gmail.com>
2016-11-30 09:50:42 -0500

    set up server and webpack

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict'
+require('dotenv').config({ silent: true });
+const bodyParser  = require('body-parser');
+const express     = require('express');
+const logger      = require('morgan');
+const path        = require('path');
+const jwt         = require('jsonwebtoken');
+const expressJWT  = require('express-jwt');
+const app         = express();
+const PORT        = process.argv[2] || process.env.port || 3000;
+
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+// app.use(expressJWT({secret: process.env.secret}).unless(
+//   {path: ['/favicon.ico', '/user/signup', '/user/login', '/api/storefronts', '/api/myStorefront', '/api/storefront', '/api/items', '/search/zip']}
+// ));
+
+// const userRouter = require('./routes/user.js');
+// app.use('/user', userRouter);
+
+app.listen(PORT, () => {console.log('Overpriced organic artisanal tacos on port', PORT)});
---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import React    from 'react'
+import ReactDOM from 'react-dom'
+import App      from './Components/00App/App.jsx'
+
+// mount our App at #container
+ReactDOM.render(<App />, document.querySelector('#root-container'),
+
+);
